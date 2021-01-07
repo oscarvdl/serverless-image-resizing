e2749f6a648cc2ee39dc3225150ee482e904d555
Oscar van der Leij <oscar.vanderleij@gmail.com>
2021-01-07 17:19:11 +0100

    Added webp support

--- lambda/index.js
+++ lambda/index.js
@@ -5,10 +5,21 @@ const S3 = new AWS.S3({
   signatureVersion: 'v4',
 });
 const Sharp = require('sharp');
+const path = require('path');
 
 const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
 const ALLOWED_DIMENSIONS = new Set();
+const EXTENSIONS = {
+  '.jpg': {
+    format: 'jpeg',
+    contentType: 'image/jpeg'
+  },
+  '.webp': {
+    format: 'webp',
+    contentType: 'image/webp'
+  }
+}
 
 if (process.env.ALLOWED_DIMENSIONS) {
   const dimensions = process.env.ALLOWED_DIMENSIONS.split(/\s*,\s*/);
@@ -22,8 +33,11 @@ exports.handler = function(event, _context, callback) {
   const width = parseInt(match[2], 10);
   const height = parseInt(match[3], 10);
   const originalKey = match[4];
+  const basename = path.basename(originalKey);
+  const extension = path.extname(originalKey);
+  console.log(basename)
 
-  if (ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
+  if ((ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) || typeof EXTENSIONS[extension] === 'undefined') {
      callback(null, {
       statusCode: '403',
       headers: {},
@@ -38,26 +52,26 @@ exports.handler = function(event, _context, callback) {
     body: '',
   });
   
-    S3.headObject({Bucket: BUCKET, Key: key}).promise()
-    .then(_data => redirectToKeyLocation())
-    .catch(err => {
-      if (err && err.code == 'Forbidden') {
-        S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-          .then(data => Sharp(data.Body)
-            .resize(width, height, { fit: Sharp.fit.inside })
-            .toFormat('jpeg')
-            .toBuffer()
-          )
-          .then(buffer => S3.putObject({
-            Body: buffer,
-            Bucket: BUCKET,
-            ContentType: 'image/jpeg',
-            Key: key,
-          }).promise()
-          .then(() => redirectToKeyLocation())
-          .catch(err => callback(err))
+  S3.headObject({Bucket: BUCKET, Key: key}).promise()
+  .then(_data => redirectToKeyLocation())
+  .catch(err => {
+    if (err && err.code == 'Forbidden') {
+      S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
+        .then(data => Sharp(data.Body)
+          .resize(width, height, { fit: Sharp.fit.inside })
+          .toFormat(EXTENSIONS[extension].format)
+          .toBuffer()
         )
-      }
-    })
-    .catch(err => callback(err))
+        .then(buffer => S3.putObject({
+          Body: buffer,
+          Bucket: BUCKET,
+          ContentType: EXTENSIONS[extension].contentType,
+          Key: key,
+        }).promise()
+        .then(() => redirectToKeyLocation())
+        .catch(err => callback(err))
+      )
+    }
+  })
+  .catch(err => callback(err))
 }
